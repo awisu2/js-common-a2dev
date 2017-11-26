3b13ec1c8a7bd1d1f427360cf1204ab168e2974f
awisu2 <awisu2@gmail.com>
2017-11-26 22:41:28 +0900

    Tasksの取得を可能に修正

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   Common: require('./dist/Common.js').default,
   Http: require('./dist/Http.js').default,
   File: require('./dist/File.js').default,
-  MochaTester: require('./dist/MochaTester.js').default
+  MochaTester: require('./dist/MochaTester.js').default,
+  Tasks: require('./dist/Tasks.js').default
 }
